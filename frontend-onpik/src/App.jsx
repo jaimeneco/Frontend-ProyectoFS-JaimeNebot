44f37c1076fa7a6d21c96e857cb9d98188d2dc9f
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-04-15 15:39:10 +0200

    up

--- frontend-onpik/src/App.jsx
+++ frontend-onpik/src/App.jsx
@@ -1,29 +1,58 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 // Components:
-import {Header} from './components/Header'
-import {Footer} from './components/Footer'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 // Pages
-import {Home} from '../src/pages/home/Home' 
-import {AboutUs} from '../src/pages/aboutUs/AboutUs'
-import {Mountain} from '../src/pages/mountain/Mountain'
-import {Sea} from '../src/pages/sea/Sea'
+import { Login } from ''
+import { Registro } from ''
+import { Home } from '../src/pages/home/Home'
+import { Mountain } from '../src/pages/mountain/Mountain'
+import { Sea } from '../src/pages/sea/Sea'
+import { AboutUs } from '../src/pages/aboutUs/AboutUs'
+import { Carrito } from '';
+import { Perfil } from '';
+import {Error} from '';
+
+
+  // PAGES EXPORRTADAS
 
 
 
-function App() {
-  return (
-    <>
-    <Header /> 
-    <Home />
-    <div>
-      <h1>Hola, somos ONPIK</h1>
-    </div>
-    <Footer />
-    </>
-  );
+  function App() {
+    return (
+        <BrowserRouter>
+
+          <>
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='/registro' element={<Registro />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/mountain' element={<Mountain />} />
+              <Route path='/sea' element={<Sea />} />
+              <Route path='/aboutus' element={<AboutUs />} />
+              <Route path='/carrito' element={<Carrito />} />
+              <Route path='/perfil' element={<Perfil />} />
+
+
+              {/* Añadir subruta dentro de ruta */}
+              {/* <Route path='/actividades/*' element={<Outlet />}>
+              <Route path='rafting' element={<Rafting />} />
+              <Route path='vias_ferratas' element={<ViasFerratas />} />
+              <Route path='vuelos_en_globo' element={< VuelosGlobo />} />
+              <Route path='parapente' element={<Parapente />} />
+              <Route path='barranquismo' element={<Barranquismo />} />
+              <Route path='escalada' element={<Escalada />} />
+              <Route path='nopage' element={<Vacia />} /> */}
+
+            </Routes>
+
+          </>
+          </BrowserRouter>
+      
+          );
 }
-
 export default App;
--- frontend-onpik/src/pages/aboutUs/AboutUs.jsx
+++ frontend-onpik/src/pages/aboutUs/AboutUs.jsx
@@ -1,3 +1,5 @@
+
+
 export const AboutUs = () => {
     return ( 
         <p>Aquí está el apartado About Us</p>
