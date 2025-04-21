90e0125327c06f538fe8f8d1d7c931e9f6717bd4
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-04-21 19:17:34 +0200

    up

--- frontend-onpik/src/components/footer/Footer.jsx
+++ frontend-onpik/src/components/footer/Footer.jsx
@@ -28,7 +28,7 @@ export const Footer = () => {
                 <div className="Footer-content">
                     <div className="Footer-logo">
                         <NavLink to="/">
-                            <img src="src/public/imgs/logo-footer.png" alt="Logotipo ONPIK" title='ONPIK' className="Footer-logoImage" loading='lazy' />
+                            <img src="src/public/imgs/img-footer/logo-footer.png" alt="Logotipo ONPIK" title='ONPIK' className="Footer-logoImage" loading='lazy' />
                         </NavLink>
                     </div>
 
