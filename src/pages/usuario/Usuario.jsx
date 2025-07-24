9ceb659bb40f8afd7640e439ca689d4d932e3454
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-07-24 14:00:10 +0200

    up

--- src/pages/usuario/Usuario.jsx
+++ src/pages/usuario/Usuario.jsx
@@ -49,7 +49,7 @@ export const Usuario = () => {
                 <div className="Usuario-card">
                     <img
                         className="Usuario-img"
-                        src={usuario?.imagen || '/default-avatar.jpg'}
+                        src={usuario?.imagen || 'imgs/avatar-porDefecto.jpg'}
                         alt="Imagen de perfil"
                     />
                     <p className="Usuario-text">Nombre: {usuario?.name || 'Cargando...'}</p>
@@ -63,55 +63,3 @@ export const Usuario = () => {
         </>
     );
 };
-
-
-//     return (
-//         <>
-//             <Header />
-//             <main className="Usuario-main">
-//                 <p className="Usuario-mainTitle">¡Nos alegramos de tenerte por aquí!</p>
-
-//                 <div className="Usuario-card">
-//                     <img
-//                         className="Usuario-img"
-//                         src={usuario?.img || '/default-avatar.jpg'}
-//                         alt="Imagen de perfil"
-//                     />
-//                     <p className="Usuario-text">Nombre: {usuario?.nombre || 'Cargando...'}</p>
-//                     <p className="Usuario-text">Ciudad: {usuario?.ciudad || 'Cargando...'}</p>
-//                     <p className="Usuario-text">País: {usuario?.pais || 'Cargando...'}</p>
-//                     <button className="Usuario-button" onClick={handleLogout}>
-//                         Cerrar sesión
-//                     </button>
-//                 </div>
-//             </main>
-//             <Footer />
-//         </>
-//     );
-// };
-
-
-// import './Usuario.css';
-// import { Header } from '../../components/header/Header'
-// import { Footer } from '../../components/footer/Footer'
-
-
-// export const Usuario = () => {
-//     return (
-//         <>
-//             <Header />
-//             <main className='Usuario-main'>
-//                 <p className='Usuario-mainTitle'>¡Nos alegramos de tenerte por aquí!</p>
-//                 <div className='Usuario-card'>
-//                     <img className='Usuario-img' src="/" alt="Img perfil" />
-//                     <p className='Usuario-text'>Nombre de usuario:</p>
-//                     <p className='Usuario-text'>Ciudad:</p>
-//                     <p className='Usuario-text'>País:</p>
-//                     <button className='Usuario-button'>Cerrar sesión</button>
-//                 </div>
-                
-//             </main>
-//             <Footer />
-//         </>
-//     );
-// }
